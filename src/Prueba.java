b872c2bc60b7f7178304fbfe3db6d501e282af45
Armando Enriquez <armandoenriquez0531@gmail.com>
2022-02-22 17:14:59 -0600

    Se agrego el método llenado y se agregaron los primeros JLabel

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,17 +2,39 @@ import java.awt.*;
 import javax.swing.*;
 
 class VentanaInicio extends JFrame{
+	
+	GridBagLayout gbl=new GridBagLayout();
+	GridBagConstraints gbc=new GridBagConstraints();
 
 	public VentanaInicio() {
 
-		getContentPane().setLayout(null);
+		getContentPane().setLayout(gbl);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Ventana principal");
 		setSize(500, 300);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		
+		JLabel label1=new JLabel("The Classic Form includes all visible fields for");
+		llenado(GridBagConstraints.HORIZONTAL, 0,0, 1, 3, label1);
+		
+		JLabel label2=new JLabel("this list");
+		llenado(GridBagConstraints.HORIZONTAL, 0, 1, 1, 1, label2);
+		
+		JLabel label3=new JLabel("incluide the folowing");
+		llenado(GridBagConstraints.HORIZONTAL, 0,4, 1, 2, label3);
+		
 
+	}
+	
+	public void llenado(int rellenado,int x, int y,int altura,int largo,Component componente) {
+		gbc.gridx= x;
+		gbc.gridy=y;
+		gbc.gridheight=altura;
+		gbc.gridwidth=largo;
+		gbc.fill=rellenado;
+		gbl.setConstraints(componente, gbc);
+		add(componente);
 	}
 }
 
